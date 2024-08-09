b47b07ff006f9ef4bcbaf90b115a73af42ff68be
DRypa <dr.drypa.k@mail.ru>
2024-08-09 22:06:54 +0300

    add second dictionary
    lines processing via chan

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"unicode/utf8"
 )
 
-var uniqueLines = make(map[string]struct{})
-
-func loadWords(url string) {
+func loadWords(url string, lines chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done() // Уменьшаем счетчик горутин
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Download dictionary failed:", err)
@@ -25,29 +25,33 @@ func loadWords(url string) {
 		fmt.Println("Download dictionary failed. Status Code:", resp.StatusCode)
 		return
 	}
-	scanner := bufio.NewScanner(resp.Body)
 
+	scanner := bufio.NewScanner(resp.Body)
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		if line != "" {
-			uniqueLines[line] = struct{}{}
-		}
+		lines <- line
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Read dictionary error:", err)
-		return
 	}
 }
 
-func selectLinesByLength(length int) []string {
-	var result []string
-	for line := range uniqueLines {
-		if utf8.RuneCountInString(line) == length {
-			result = append(result, line)
+func filterByLength(lines chan string, length int, filtered chan<- string) {
+	for l := range lines {
+		if utf8.RuneCountInString(l) == length {
+			filtered <- l
 		}
 	}
-	return result
+	close(filtered) // Закрываем канал после обработки
+}
+
+func selectUniqueLines(strings <-chan string) map[string]struct{} {
+	uniqueStrings := make(map[string]struct{})
+	for str := range strings {
+		uniqueStrings[str] = struct{}{}
+	}
+	return uniqueStrings
 }
 
 func writeToFile(lines []string, fileName string) error {
@@ -67,6 +71,7 @@ func writeToFile(lines []string, fileName string) error {
 
 	return writer.Flush()
 }
+
 func readFromFile(fileName string) []string {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -84,34 +89,64 @@ func readFromFile(fileName string) []string {
 }
 
 func main() {
-
 	url := "https://raw.githubusercontent.com/LussRus/Rus_words/master/cp1251/txt/nouns/summary.txt"
+	url2 := "https://gist.githubusercontent.com/kissarat/bd30c324439cee668f0ac76732d6c825/raw/147eecc9a86ec7f97f6dd442c2eda0641ddd78dc/russian-mnemonic-words.txt"
 	letters5TmpPath := "/var/tmp/letters5"
+	lettersChan := make(chan string)
+	letters5chan := make(chan string)
 	var letters5 []string
+
 	if _, err := os.Stat(letters5TmpPath); err == nil {
 		letters5 = readFromFile(letters5TmpPath)
 	} else {
-		loadWords(url)
-		fmt.Printf("Unique lines: %d\n", len(uniqueLines))
-		letters5 = selectLinesByLength(5)
-		fmt.Printf("Number of lines of 5 letters: %d\n", len(letters5))
+		var wg sync.WaitGroup
+
+		wg.Add(2) // Добавляем количество горутин для загрузки словарей
+		go loadWords(url, lettersChan, &wg)
+		go loadWords(url2, lettersChan, &wg)
+
+		go func() {
+			wg.Wait()          // Ждем завершения всех загрузок
+			close(lettersChan) // Закрываем канал после завершения всех горутин
+		}()
+
+		go filterByLength(lettersChan, 5, letters5chan)
+
+		uniqueLines := selectUniqueLines(letters5chan)
+
+		letters5 = make([]string, len(uniqueLines), len(uniqueLines))
+		for line := range uniqueLines {
+			letters5 = append(letters5, line)
+		}
+		fmt.Printf("Unique 5-letter lines: %d\n", len(uniqueLines))
+
 		err = writeToFile(letters5, letters5TmpPath)
 		if err != nil {
 			fmt.Println("Failed to save temp file")
 		}
 	}
 
-	notContains := []rune{'б', 'у', 'к', 'в', 'п', 'о', 'з', 'е', 'д', 'с'}
-	contain := []rune{'р', 'и', 'а'}
+	notContains := []rune{'б', 'у', 'к', 'в', 'с', 'е', 'м', 'ь', 'я', 'р', 'о', 'з'}
+	contain := []rune{'п', 'а'}
 	correctPositions := []solver.RunePlace{
 		{Rune: 'а', Pos: 4},
+		{Rune: 'п', Pos: 0},
+	}
+	incorrectPositions := []solver.RunePlace{
+		{Rune: 'у', Pos: 1},
+		{Rune: 'в', Pos: 3},
+		{Rune: 'а', Pos: 4},
+		{Rune: 'с', Pos: 0},
+		{Rune: 'а', Pos: 1},
+		{Rune: 'у', Pos: 3},
+		{Rune: 'с', Pos: 4},
 	}
 
 	s := solver.NewSolver(letters5, 5)
 	s.Contains(contain)
 	s.NotContain(notContains)
 	s.CorrectRunePlaces(correctPositions)
-
+	s.IncorrectRunePlaces(incorrectPositions)
 	results := s.GetSuitable()
 	fmt.Println("Matching lines:")
 	if len(results) == 0 {
@@ -121,5 +156,4 @@ func main() {
 			fmt.Println(el)
 		}
 	}
-
 }

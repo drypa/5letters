c3ca2adbc1cf9858bde225d26e1a72827c9001e6
drypa <dr.drypa.k@mail.ru>
2024-10-04 20:31:15 +0300

    IncorrectRunePlaces tests added

--- solver/solver_test.go
+++ solver/solver_test.go
@@ -4,26 +4,37 @@ import (
 	"testing"
 )
 
+var allWords = []string{
+	"abcde",
+	"aaaaa",
+	"bcdef",
+	"cdefg",
+	"defgh",
+}
+
+type testCase struct {
+	incorrectPlaces []RunePlace
+	resultsCount    int
+	name            string
+}
+
 func TestSolver_IncorrectRunePlaces(t *testing.T) {
-	type TestCase struct {
-		incorrectPlaces []RunePlace
-		resultsCount    int
-		name            string
-	}
 
-	allWords := []string{
-		"abcde",
-		"aaaaa",
-		"bcdef",
-		"cdefg",
-		"defgh",
-	}
-
-	tests := []TestCase{
+	tests := []testCase{
 		{
 			incorrectPlaces: make([]RunePlace, 0),
 			resultsCount:    len(allWords),
 			name:            "Empty incorrect places collection",
+		},
+		{
+			incorrectPlaces: []RunePlace{{Rune: 'z', Pos: 0}},
+			resultsCount:    len(allWords),
+			name:            "Not contained rune",
+		},
+		{
+			incorrectPlaces: []RunePlace{{Rune: 'z', Pos: 0}, {Rune: 'z', Pos: 1}},
+			resultsCount:    len(allWords),
+			name:            "Many not contained runes",
 		},
 		{
 			incorrectPlaces: []RunePlace{{Rune: 'a', Pos: 0}},
@@ -52,9 +63,45 @@ func TestSolver_IncorrectRunePlaces(t *testing.T) {
 			s := NewSolver(allWords, 5)
 			s.IncorrectRunePlaces(tt.incorrectPlaces)
 			if suitable := s.GetSuitable(); len(suitable) != tt.resultsCount {
-				t.Errorf("GetSuitable() = %d, want %d", len(suitable), tt.resultsCount)
+				t.Errorf("GetSuitable() len = %d, want %d", len(suitable), tt.resultsCount)
 			}
 		})
 	}
 
+}
+
+func TestSolver_IncorrectRunePlaces_ConsistentlyAdd(t *testing.T) {
+	tests := []testCase{
+		{
+			incorrectPlaces: []RunePlace{{Rune: 'a', Pos: 0}, {Rune: 'b', Pos: 0}},
+			resultsCount:    2,
+			name:            "Incorrect places on same position",
+		},
+		{
+			incorrectPlaces: []RunePlace{{Rune: 'a', Pos: 0}, {Rune: 'b', Pos: 1}},
+			resultsCount:    3,
+			name:            "Incorrect places on different positions",
+		},
+		{
+			incorrectPlaces: []RunePlace{{Rune: 'f', Pos: 0}, {Rune: 'z', Pos: 1}},
+			resultsCount:    len(allWords),
+			name:            "Many not contained runes",
+		},
+		{
+			incorrectPlaces: []RunePlace{{Rune: 'h', Pos: 4}, {Rune: 'f', Pos: 3}},
+			resultsCount:    3,
+			name:            "Many not contained runes",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewSolver(allWords, 5)
+			for _, r := range tt.incorrectPlaces {
+				s.IncorrectRunePlaces([]RunePlace{r})
+			}
+			if suitable := s.GetSuitable(); len(suitable) != tt.resultsCount {
+				t.Errorf("GetSuitable() len = %d, want %d", len(suitable), tt.resultsCount)
+			}
+		})
+	}
 }
